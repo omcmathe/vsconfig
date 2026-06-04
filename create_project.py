955d4d29cf256ce3a9676f49e937b1f01c296c91
Matt <account@mathss.net>
2026-06-04 16:32:55 +0100

    update workspace and settings.json configs

--- create_project.py
+++ create_project.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from subprocess import run as run_command
 import json
 
 
@@ -9,30 +8,38 @@ CWD_NAME = CWD.name
 
 WORKSPACE_FILE_TEMPLATE = {
     "folders": [
-        {
-            "path": "..",
-            "name": CWD_NAME
-        },
-        {
-            "path": "../.vscode",
-            "name": ".vscode"
-        }
+        {"path": "..", "name": CWD_NAME},
+        {"path": "scratchpads", "name": "Scratchpads"},
     ],
-    "settings": {}
+    "settings": {
+        "files.exclude": {
+            "**/.vscode": True,
+        }
+    },
 }
 
 WORKSPACE_FILE_EXT = ".code-workspace"
 
-SETTINGS_TEMPLATE = {"files.exclude": {"**/.vscode": True, "**/.git": True, "**/.svn": True, "**/.hg": True, "**/.DS_Store": True, "**/Thumbs.db": True}}
+SETTINGS_TEMPLATE = {
+    "files.exclude": {
+        "**/.git": True,
+        "**/.svn": True,
+        "**/.hg": True,
+        "**/.DS_Store": True,
+        "**/Thumbs.db": True,
+    }
+}
 
 
 def main() -> None:
     new_vsc_folder = CWD / ".vscode"
+    scratchpads_folder = new_vsc_folder / "scratchpads"
     settings_file = new_vsc_folder / "settings.json"
     workspace_file = new_vsc_folder / f"{CWD_NAME}{WORKSPACE_FILE_EXT}"
     
     try:
         new_vsc_folder.mkdir(exist_ok=True)
+        scratchpads_folder.mkdir(exist_ok=True)
         settings_file.write_text(json.dumps(SETTINGS_TEMPLATE, indent=2))
         workspace_file.write_text(json.dumps(WORKSPACE_FILE_TEMPLATE, indent=2))
     except Exception:
